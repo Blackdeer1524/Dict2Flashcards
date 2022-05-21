cdf31a23ed2ed3b37587c7e9a0b8be48729168e3
Danila Slesarev <blackdeer1524@yandex.ru>
2022-05-21 18:12:16 +0300

    feat: added error handling in web_sentencedict.py

--- parsers/sentence_parsers/web_sentencedict.py
+++ parsers/sentence_parsers/web_sentencedict.py
@@ -2,31 +2,36 @@ import requests
 import bs4
 import re
 from typing import Iterator
+import os
+from utils.error_handling import create_exception_message
 
 
-def get_sentence_batch(word: str, step: int = 5) -> Iterator[tuple[list, bool]]:
+FILE_PATH = os.path.dirname(__file__)
+
+
+def get_sentence_batch(word: str, step: int = 5) -> Iterator[tuple[list, str]]:
     re_pattern = re.compile("^(.?\d+.? )")
     try:
         page = requests.get(f"https://sentencedict.com/{word}.html")
-        st_code = page.status_code
-        if st_code == 200:
-            soup = bs4.BeautifulSoup(page.content, "html.parser")
-        else:
-            return [], True
+        page.raise_for_status()
+        soup = bs4.BeautifulSoup(page.content, "html.parser")
         sentences_block = soup.find("div", {"id": "all"})
-        if sentences_block is not None:
-            sentences_block = sentences_block.find_all("div")
-            sentences = []
-            for sentence in sentences_block:
-                if sentence is not None:
-                    text = sentence.get_text().strip()
-                    if text:
-                        text = re.sub(re_pattern, "", text)
-                        sentences.append(text)
-            sentences.sort(key=len)
-            for i in range(0, len(sentences), step):
-                yield sentences[i:i + step], False
-        else:
-            yield [], False
-    except Exception:
-        yield [], True
+        if sentences_block is None:
+            yield [], f"{FILE_PATH} couldn't parse a web page!"
+            return
+
+        sentences_block = sentences_block.find_all("div")
+        sentences = []
+        for sentence in sentences_block:
+            if sentence is not None:
+                text = sentence.get_text().strip()
+                if text:
+                    text = re.sub(re_pattern, "", text)
+                    sentences.append(text)
+
+        sentences.sort(key=len)
+        for i in range(0, len(sentences), step):
+            yield sentences[i:i + step], False
+    except Exception as e:
+        yield [], create_exception_message()
+        return

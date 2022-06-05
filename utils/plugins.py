33c75d60c55302a7f9d42e7100d01cc3179c486d
Danila Slesarev <blackdeer1524@yandex.ru>
2022-06-05 14:14:16 +0300

    feat(plugins): parser factory implemented

--- utils/plugins.py
+++ utils/plugins.py
@@ -1,12 +1,23 @@
 import importlib
 import pkgutil
 from dataclasses import dataclass
+from typing import Callable
 
-from utils.storages import FrozenDict
 import parsers.image_parsers
+import parsers.sentence_parsers
 import parsers.word_parsers.local
 import parsers.word_parsers.web
-import parsers.sentence_parsers
+from parsers.return_types import ImageGenerator, SentenceGenerator
+from utils.cards import WebCardGenerator, LocalCardGenerator
+from utils.storages import FrozenDict
+
+
+class PluginError(Exception):
+    pass
+
+
+class UnknownPluginName(PluginError):
+    pass
 
 
 @dataclass(init=False, frozen=True, repr=False)
@@ -31,16 +42,18 @@ class PluginLoader:
                 res[parser_trunc_name] = importlib.import_module(name)
             return res
 
-        super().__setattr__("web_word_parsers", FrozenDict({name: (module.translate, module.define)
+        super().__setattr__("web_word_parsers", FrozenDict({name: {"item_converter": module.translate,
+                                                                   "parsing_function": module.define}
                                                             for name, module in
                                                             parse_namespace(parsers.word_parsers.web).items()}))
-        super().__setattr__("local_word_parsers", FrozenDict({name: (module.translate, module.DICTIONARY_PATH)
+        super().__setattr__("local_word_parsers", FrozenDict({name: {"item_converter": module.translate,
+                                                                     "local_dict_path": module.DICTIONARY_PATH}
                                                               for name, module in
                                                               parse_namespace(parsers.word_parsers.local).items()}))
-        super().__setattr__("web_sent_parsers", FrozenDict({name: (module.get_sentence_batch)
+        super().__setattr__("web_sent_parsers", FrozenDict({name: module.get_sentence_batch
                                                             for name, module in
                                                             parse_namespace(parsers.sentence_parsers).items()}))
-        super().__setattr__("image_parsers", FrozenDict({name: (module.get_image_links)
+        super().__setattr__("image_parsers", FrozenDict({name: module.get_image_links
                                                          for name, module in
                                                          parse_namespace(parsers.image_parsers).items()}))
 
@@ -53,6 +66,26 @@ class PluginLoader:
                                self.web_sent_parsers.keys(), self.image_parsers.keys())):
             res += f"\n{name}{indent}{indent.join(keys)}"
         return res
+
+    def get_web_card_generator(self, name: str) -> WebCardGenerator:
+        if (args := self.web_word_parsers.get(name)) is None:
+            raise UnknownPluginName("Unknown WebCardGenerator!")
+        return WebCardGenerator(**args)
+
+    def get_local_card_generator(self, name: str) -> LocalCardGenerator:
+        if (args := self.local_word_parsers.get(name)) is None:
+            raise UnknownPluginName("Unknown LocalCardGenerator!")
+        return LocalCardGenerator(**args)
+
+    def get_sentence_parser(self, name: str) -> Callable[[str, int], SentenceGenerator]:
+        if (gen := self.web_sent_parsers.get(name)) is None:
+            raise UnknownPluginName("Unknown sentence parser!")
+        return gen
+
+    def get_image_parser(self, name: str) -> Callable[[str], ImageGenerator]:
+        if (gen := self.image_parsers.get(name)) is None:
+            raise UnknownPluginName("Unknown image parser!")
+        return gen
 
 
 plugins = PluginLoader()

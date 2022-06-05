58dffe3cf0ead311568fc7f7d329b48562328196
Danila Slesarev <blackdeer1524@yandex.ru>
2022-06-05 14:28:59 +0300

    chores: var rename

--- consts/paths.py
+++ consts/paths.py
@@ -2,16 +2,19 @@ from pathlib import Path
 import os
 from platform import system
 
-HISTORY_FILE_PATH = Path("./history.json")
-CONFIG_FILE_PATH = Path("./config.json")
+
+CURRENT_WORKING_DIR = Path(os.getcwd())
+LOCAL_MEDIA_DIR = CURRENT_WORKING_DIR / "media"
+HISTORY_FILE_PATH = CURRENT_WORKING_DIR / "history.json"
+CONFIG_FILE_PATH = CURRENT_WORKING_DIR / "config.json"
 USER_FOLDER = Path(os.path.expanduser("~"))
-CURRENT_SYSTEM = system()
-if CURRENT_SYSTEM == "Linux":
-    STANDARD_ANKI_MEDIA = USER_FOLDER / ".local/share/Anki2/"
-elif CURRENT_SYSTEM == "Windows":
-    STANDARD_ANKI_MEDIA = USER_FOLDER / "AppData/Roaming/Anki2/"
+SYSTEM = system()
+if SYSTEM == "Linux":
+    MEDIA_DOWNLOADING_LOCATION = USER_FOLDER / ".local/share/Anki2/"
+elif SYSTEM == "Windows":
+    MEDIA_DOWNLOADING_LOCATION = USER_FOLDER / "AppData/Roaming/Anki2/"
 else:
-    STANDARD_ANKI_MEDIA = "./"
-STANDARD_ANKI_MEDIA = Path(STANDARD_ANKI_MEDIA)
-if not STANDARD_ANKI_MEDIA.exists():
-    STANDARD_ANKI_MEDIA = Path("./")
+    MEDIA_DOWNLOADING_LOCATION = CURRENT_WORKING_DIR
+
+if not MEDIA_DOWNLOADING_LOCATION.exists():
+    MEDIA_DOWNLOADING_LOCATION = CURRENT_WORKING_DIR

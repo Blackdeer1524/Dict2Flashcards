add4bbeeac321447b34f47c68c1a8ec401003648
Danila Slesarev <blackdeer1524@yandex.ru>
2022-06-10 19:02:34 +0300

    chores: starter implementing local audio getters

---
+++ parsers/local_audio_getters/POS_cambridge_uk.py
@@ -0,0 +1,22 @@
+import os
+from utils.string_utils import remove_special_chars
+from consts.paths import LOCAL_MEDIA_DIR
+
+
+LETTERS = frozenset("abcdefghijklmnopqrstuvwxyz")
+AUDIO_NAME_SPEC_CHARS = '/\\:*?\"<>| '
+AUDIO_FOLDER = "uk_audios"
+
+
+def get_local_audio_path(word: str, dict_tags: dict):
+    word = word.strip().lower()
+    pos = dict_tags.get("pos", "")
+    if not word or not pos:
+        return ""
+
+    letter_group = word[0] if word[0] in LETTERS else "0-9"
+    name = f"{remove_special_chars(word.lower(), '-', AUDIO_NAME_SPEC_CHARS)}.mp3"
+    search_root = os.path.join(LOCAL_MEDIA_DIR, AUDIO_FOLDER, letter_group)
+    pos = remove_special_chars(pos.lower(), '-', AUDIO_NAME_SPEC_CHARS)
+    res = os.path.join(search_root, pos, name)
+    return res if os.path.exists(res) else ""
---
+++ parsers/local_audio_getters/cambridge_uk.py
@@ -0,0 +1,23 @@
+import os
+from utils.string_utils import remove_special_chars
+from consts.paths import LOCAL_MEDIA_DIR
+
+
+LETTERS = frozenset("abcdefghijklmnopqrstuvwxyz")
+AUDIO_NAME_SPEC_CHARS = '/\\:*?\"<>| '
+AUDIO_FOLDER = "uk_audios"
+
+
+def get_local_audio_path(word, dict_tags: dict):
+    word = word.strip().lower()
+    if not word:
+        return ""
+
+    letter_group = word[0] if word[0] in LETTERS else "0-9"
+    name = f"{remove_special_chars(word.lower(), '-', AUDIO_NAME_SPEC_CHARS)}.mp3"
+    search_root = os.path.join(LOCAL_MEDIA_DIR, AUDIO_FOLDER, letter_group)
+
+    for current_dir_path, dirs, files in os.walk(search_root):
+        if name in files:
+            return os.path.join(current_dir_path, name)
+    return ""

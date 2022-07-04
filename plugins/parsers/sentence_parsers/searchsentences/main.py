7ba174f49d38ee6e725f6f2d087969089b76aac4
Danila Slesarev <blackdeer1524@yandex.ru>
2022-07-05 02:01:52 +0300

    fix(sentence_parsers): error message fix

--- plugins/parsers/sentence_parsers/searchsentences/main.py
+++ plugins/parsers/sentence_parsers/searchsentences/main.py
@@ -7,7 +7,7 @@ import requests
 from plugins_management.config_management import Config
 from plugins_management.parsers_return_types import SentenceGenerator
 
-FILE_PATH = os.path.basename(__file__)
+FILE_PATH = os.path.split(os.path.dirname(__file__))[-1]
 
 _CONF_VALIDATION_SCHEME = {}
 

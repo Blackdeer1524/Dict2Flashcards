9d85f43738ee90334aebd612b650234df5f636a9
Danila Slesarev <blackdeer1524@yandex.ru>
2022-07-24 00:39:52 +0300

    fix(forvo): fixed forvo audio getter

--- plugins/parsers/audio_getters/web/forvo/main.py
+++ plugins/parsers/audio_getters/web/forvo/main.py
@@ -8,7 +8,7 @@ import requests.utils
 
 from plugins_management.config_management import LoadableConfig
 from .consts import _PLUGIN_NAME, _PLUGIN_LOCATION
-from .page_processing import get_forvo_page, get_forvo_audio_link
+from .page_processing import get_forvo_page, get_audio_link
 
 _CONFIG_DOCS = """
 language_code
@@ -34,21 +34,22 @@ def get_web_audios(word: str, dict_tags: dict) -> tuple[tuple[list[str], list[st
     audio_links = []
     additional_info = []
     wordEncoded = requests.utils.requote_uri(word)
-    forvoPage, error_message = get_forvo_page("https://forvo.com/word/" + wordEncoded, config["timeout"])
+    forvoPage, error_message = get_forvo_page("https://forvo.com/word/" + wordEncoded, timeout=config["timeout"])
     if error_message:
-        return (([], []), error_message)
+        return ([], []), error_message
     speachSections = forvoPage.select("div#language-container-" + config["language_code"])
     if not len(speachSections):
-        return (([], []), f"[{_PLUGIN_NAME}] Word not found (Language Container does not exist!)")
+        return ([], []), f"[{_PLUGIN_NAME}] Word not found (Language Container does not exist!)"
     speachSections = forvoPage.select_one("div#language-container-" + config["language_code"])
-    audioListUl = speachSections.select_one("ul.show-all-pronunciations")
+    audioListUl = speachSections.select_one("ul")
     if audioListUl is None or not len(audioListUl.findChildren(recursive=False)):
-        return (([], []), f"[{_PLUGIN_NAME}] Word not found (Language Container exists, but audio not found)")
+        return ([], []), f"[{_PLUGIN_NAME}] Word not found (Language Container exists, but audio not found)"
     if(config["language_code"] == "en"):
         audioListLis = forvoPage.select("li[class*=en_]")
     else:
-        audioListLis = audioListUl.find_all("li",attrs={'class': None} )
+        audioListLis = audioListUl.find_all("li", attrs={'class': None})
     for li in audioListLis:
-        audio_links.append(get_forvo_audio_link(li))
-        additional_info.append(li.get("class", ""))
-    return ((audio_links, additional_info), "")
+        if (r := li.find("div")) is not None and (onclick := r.get("onclick")) is not None:
+            audio_links.append(get_audio_link(onclick))
+            additional_info.append(li.get("class", ""))
+    return (audio_links, additional_info), ""

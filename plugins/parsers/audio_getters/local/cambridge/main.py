7d07ae9b3ebce1a076df300d102d0432d8ba494d
Danila Slesarev <blackdeer1524@yandex.ru>
2022-08-01 22:52:01 +0300

    feat(cambridge): improved additional info provided by local audio cambridge

--- plugins/parsers/audio_getters/local/cambridge/main.py
+++ plugins/parsers/audio_getters/local/cambridge/main.py
@@ -44,34 +44,51 @@ def get_audios(word, card_data: dict) -> AudioData:
     search_root = os.path.join(LOCAL_MEDIA_DIR, audio_folder, letter_group)
 
     if config["pos_search"]:
-        pos = remove_special_chars(card_data.get(FIELDS.dict_tags, {})
-                                            .get("pos", "")
-                                            .lower(),
-                                   '-', _AUDIO_NAME_SPEC_CHARS)
-        res = os.path.join(search_root, pos, name)
-        if os.path.exists(res):
-            if os.path.isfile(res):
-                return ([res], [name]), ""
-            if os.path.isdir(res):
-                file_paths = []
-                additional_info = []
-                for item in os.listdir(res):
-                    if os.path.isfile((file_path := os.path.join(res, item))):
-                        file_paths.append(file_path)
-                        additional_info.append(item)
-        return ([], []), ""
-
-    for current_dir_path, dirs, files in os.walk(search_root):
-        if name in files:
-            return ([os.path.join(str(current_dir_path), name)], [name]), ""
-        elif name in dirs:
-            dir = os.path.join(str(current_dir_path), name)
+        pos = card_data.get(FIELDS.dict_tags, {}).get("pos", "")
+        clear_pos = remove_special_chars(pos.lower(), '-', _AUDIO_NAME_SPEC_CHARS)
+        res = os.path.join(search_root, clear_pos, name)
+        if not os.path.exists(res):
+            return ([], []), ""
+        if os.path.isfile(res):
+            return ([res], [f"[{pos}] {os.path.splitext(name)[0]}"]), ""
+        if os.path.isdir(res):
             file_paths = []
             additional_info = []
-            for item in os.listdir(dir):
-                if os.path.isfile((file_path := os.path.join(dir, item))):
+            for item in os.listdir(res):
+                if os.path.isfile((file_path := os.path.join(res, item))):
                     file_paths.append(file_path)
-                    additional_info.append(item)
+                    additional_info.append(f"[{pos}] {os.path.splitext(item)[0]}")
             return (file_paths, additional_info), ""
+        return ([], []), ""
+
+    no_pos_location = os.path.join(search_root, name)
+    if os.path.exists(no_pos_location):
+        if os.path.isfile(no_pos_location):
+            return ([no_pos_location], [name]), ""
+        if os.path.isdir(no_pos_location):
+            file_paths = []
+            additional_info = []
+            for item in os.listdir(no_pos_location):
+                if os.path.isfile((file_path := os.path.join(no_pos_location, item))):
+                    file_paths.append(file_path)
+                    additional_info.append(os.path.splitext(item)[0])
+            return (file_paths, additional_info), ""
+    
+    pos_dirs = [directory_path for directory in os.listdir(search_root)
+                if os.path.isdir(directory_path := os.path.join(search_root, directory))]
+    for dir_root in pos_dirs:
+        for current_dir_path, dirs, files in os.walk(dir_root):
+            if name in files:
+                return ([os.path.join(str(current_dir_path), name)],
+                        [f"[{os.path.split(current_dir_path)[-1]}] {os.path.splitext(name)[0]}"]), ""
+            elif name in dirs:
+                directory = os.path.join(str(current_dir_path), name)
+                file_paths = []
+                additional_info = []
+                for item in os.listdir(directory):
+                    if os.path.isfile((file_path := os.path.join(directory, item))):
+                        file_paths.append(file_path)
+                        additional_info.append(f"[{name}] {os.path.splitext(item)[0]}")
+                return (file_paths, additional_info), ""
     return ([], []), ""
 

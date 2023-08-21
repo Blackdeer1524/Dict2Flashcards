176013799c4e960c7b1d3ff3f79d4eb719c13185
Danila Slesarev <blackdeer1524@yandex.ru>
2023-08-21 20:21:58 +0300

    chore(anki_format/random_sentence_card): removed dependency on anki addon thus made this card template crossplatform

--- src/plugins/saving/format_processors/anki_package/main.py
+++ src/plugins/saving/format_processors/anki_package/main.py
@@ -6,8 +6,7 @@ from .. import app_utils, config_management, consts, parsers_return_types
 
 
 _CONF_VALIDATION_SCHEME = {
-    "merge picked sentences in one card": (False, [bool], []), 
-    "merge separator": (" |<br><br>", [str], [])
+    "merge picked sentences in one card": (False, [bool], []),
 }
 
 
@@ -15,25 +14,21 @@ _CONFIG_DOCS = """
 merge picked sentences in one card:
     whether to merge picked sentences in one card or create different card for each sentence
     type: str
-    
-merge separator:
-    Separator used in sentence merging
-    default: " |<br><br>"
-    type: str
 """
 
 config = config_management.LoadableConfig(
     config_location=os.path.dirname(__file__),
     validation_scheme=_CONF_VALIDATION_SCHEME,
-    docs=_CONFIG_DOCS)
+    docs=_CONFIG_DOCS,
+)
 
 
 MODEL_FIELDS = [
-    {'name': 'Sentence'},
-    {'name': 'Word'},
-    {'name': 'Definition'},
-    {'name': 'Image'},
-    {'name': 'Word Audio'},
+    {"name": "Sentence"},
+    {"name": "Word"},
+    {"name": "Definition"},
+    {"name": "Image"},
+    {"name": "Word Audio"},
 ]
 
 
@@ -55,14 +50,14 @@ MODEL_CSS = """\
 
 
 ONE_SENTENCE_RESULTING_MODEL = genanki.Model(
-  1869993568,  # just a random number
-  'Mined Sentence Vocab',
-  fields=MODEL_FIELDS,
-  templates=[
-    {
-      'name': 'Recognition',
-      'qfmt': '{{Sentence}}',
-      'afmt': """\
+    1869993568,  # just a random number
+    "Mined Sentence Vocab",
+    fields=MODEL_FIELDS,
+    templates=[
+        {
+            "name": "Recognition",
+            "qfmt": "{{Sentence}}",
+            "afmt": """\
 {{FrontSide}}
 <hr id="answer">
 <div class="accent">
@@ -73,20 +68,35 @@ ONE_SENTENCE_RESULTING_MODEL = genanki.Model(
 {{Word Audio}}<br>
 
 Tags{{#Tags}}|{{/Tags}}{{Tags}}
-"""},],
-    css=MODEL_CSS)
+""",
+        },
+    ],
+    css=MODEL_CSS,
+)
 
 
-# https://ankiweb.net/shared/info/1639213385
 MERGING_RESULTING_MODEL = genanki.Model(
-  1607392319,  # just a random number
-  '[Random] Mined Sentence Vocab',
-  fields=MODEL_FIELDS,
-  templates=[
-    {
-      'name': 'Recognition',
-      'qfmt': '{{rand-alg:Sentence}}',
-      'afmt': """\
+    1607392319,  # just a random number
+    "[Random] Mined Sentence Vocab",
+    fields=MODEL_FIELDS,
+    templates=[
+        {
+            "name": "Recognition",
+            "qfmt": """\
+<span id="question">{{Sentence}}</span>
+
+<script>
+function split_and_pick(id) {
+    const element = document.getElementById(id);
+    if (element.innerHTML.includes('|')) {
+        const options = element.innerHTML.split('|');
+        element.innerHTML = options[Math.floor(Math.random() * options.length)];
+    }
+}
+split_and_pick('question');
+</script>
+""",
+            "afmt": """\
 {{Sentence}}
 <hr id="answer">
 <div class="accent">
@@ -97,15 +107,20 @@ MERGING_RESULTING_MODEL = genanki.Model(
 {{Word Audio}}<br>
 
 Tags{{#Tags}}|{{/Tags}}{{Tags}}
-"""},],
-    css=MODEL_CSS)
+""",
+        },
+    ],
+    css=MODEL_CSS,
+)
 
 
-def save(deck: app_utils.decks.SavedDataDeck,
-         saving_card_status: app_utils.decks.CardStatus,
-         saving_path: str,
-         image_names_wrapper: Callable[[str], str],
-         audio_names_wrapper: Callable[[str], str]):
+def save(
+    deck: app_utils.decks.SavedDataDeck,
+    saving_card_status: app_utils.decks.CardStatus,
+    saving_path: str,
+    image_names_wrapper: Callable[[str], str],
+    audio_names_wrapper: Callable[[str], str],
+):
     if not deck.get_card_status_stats(saving_card_status):
         return
 
@@ -122,86 +137,98 @@ def save(deck: app_utils.decks.SavedDataDeck,
         audios = ""
         hierarchical_prefix = ""
         tags = []
-        if (additional := card_page.get(app_utils.decks.SavedDataDeck.ADDITIONAL_DATA)):
-            image_paths = additional.get(app_utils.decks.SavedDataDeck.SAVED_IMAGES_PATHS, [])
+        if additional := card_page.get(app_utils.decks.SavedDataDeck.ADDITIONAL_DATA):
+            image_paths = additional.get(
+                app_utils.decks.SavedDataDeck.SAVED_IMAGES_PATHS, []
+            )
             images = " ".join([image_names_wrapper(name) for name in image_paths])
 
-            if (audio_data := additional.get(app_utils.decks.SavedDataDeck.AUDIO_DATA)) is not None:
-                audio_paths = audio_data[app_utils.decks.SavedDataDeck.AUDIO_SAVING_PATHS]
+            if (
+                audio_data := additional.get(app_utils.decks.SavedDataDeck.AUDIO_DATA)
+            ) is not None:
+                audio_paths = audio_data[
+                    app_utils.decks.SavedDataDeck.AUDIO_SAVING_PATHS
+                ]
                 audios = " ".join([audio_names_wrapper(name) for name in audio_paths])
 
-            hierarchical_prefix = additional.get(app_utils.decks.SavedDataDeck.HIERARCHICAL_PREFIX, "")
-            
-            user_tags = additional.get(app_utils.decks.SavedDataDeck.USER_TAGS, "").split()
+            hierarchical_prefix = additional.get(
+                app_utils.decks.SavedDataDeck.HIERARCHICAL_PREFIX, ""
+            )
+
+            user_tags = additional.get(
+                app_utils.decks.SavedDataDeck.USER_TAGS, ""
+            ).split()
             if hierarchical_prefix:
                 user_tags = [f"{hierarchical_prefix}::{tag}" for tag in user_tags]
             tags.extend(user_tags)
 
         saving_word = card_data.get(consts.CardFields.word, "")
         definition = card_data.get(consts.CardFields.definition, "")
-        dict_tags = card_data.get_str_dict_tags(card_data=card_data,
-                                                prefix=hierarchical_prefix,
-                                                sep="::",
-                                                tag_processor=lambda tag: app_utils.string_utils.remove_special_chars(tag, sep="_")).split()
+        dict_tags = card_data.get_str_dict_tags(
+            card_data=card_data,
+            prefix=hierarchical_prefix,
+            sep="::",
+            tag_processor=lambda tag: app_utils.string_utils.remove_special_chars(
+                tag, sep="_"
+            ),
+        ).split()
 
         tags.extend(dict_tags)
 
         if config["merge picked sentences in one card"]:
-            sentence_example = config["merge separator"].join(card_data.get(consts.CardFields.sentences, [""]))
+            sentence_example = "<br>|<br>".join(
+                card_data.get(consts.CardFields.sentences, [""])
+            )
             note = genanki.Note(
                 model=MERGING_RESULTING_MODEL,
-                # I have no idea why, but if any of the fields is empty, then card won't be added, 
-                fields=[sentence_example if sentence_example else " ",  
-                        saving_word      if saving_word      else " ",  
-                        definition       if definition       else " ",  
-                        images           if images           else " ",  
-                        audios           if audios           else " ",  
-                        ],
-                tags=tags
-                )  
+                # I have no idea why, but if any of the fields is empty, then card won't be added,
+                fields=[
+                    sentence_example if sentence_example else " ",
+                    saving_word if saving_word else " ",
+                    definition if definition else " ",
+                    images if images else " ",
+                    audios if audios else " ",
+                ],
+                tags=tags,
+            )
             anki_deck.add_note(note)
         else:
             for sentence_example in card_data.get(consts.CardFields.sentences, [""]):
                 note = genanki.Note(
                     model=ONE_SENTENCE_RESULTING_MODEL,
-                    fields=[sentence_example, 
-                            saving_word, 
-                            definition, 
-                            images, 
-                            audios],
-                    tags=tags)  
+                    fields=[sentence_example, saving_word, definition, images, audios],
+                    tags=tags,
+                )
                 anki_deck.add_note(note)
 
     my_package = genanki.Package(anki_deck)
-    my_package.write_to_file(f'{saving_path}.apkg')
+    my_package.write_to_file(f"{saving_path}.apkg")
 
 
 if __name__ == "__main__":
     import genanki
 
     my_model = genanki.Model(
-    1607392319,
-    'Simple Model',
-    fields=[
-        {'name': 'Question'},
-        {'name': 'Answer'},
-    ],
-    templates=[
-        {
-        'name': 'Card 1',
-        'qfmt': '{{rand-alg:Question}}',
-        'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-        },
-    ])
+        1607392319,
+        "Simple Model",
+        fields=[
+            {"name": "Question"},
+            {"name": "Answer"},
+        ],
+        templates=[
+            {
+                "name": "Card 1",
+                "qfmt": "{{rand-alg:Question}}",
+                "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}',
+            },
+        ],
+    )
 
     my_note = genanki.Note(
-            model=my_model,
-            fields=['Capital of Argentina', 'Buenos Aires'])
-    
-    my_deck = genanki.Deck(
-    2059400110,
-    'Country Capitals')
+        model=my_model, fields=["Capital of Argentina", "Buenos Aires"]
+    )
+
+    my_deck = genanki.Deck(2059400110, "Country Capitals")
 
     my_deck.add_note(my_note)
-    genanki.Package(my_deck).write_to_file('output.apkg')
-    
+    genanki.Package(my_deck).write_to_file("output.apkg")
